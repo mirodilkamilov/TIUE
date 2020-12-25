8aeda34aa5a213d34dfca14c98c8a64ee03683a8
Mirodil Kamilov <mirodilkamilov1999@gmail.com>
2020-12-26 03:42:17 +0500

    collaboration ready

--- root/js/carousel.js
+++ root/js/carousel.js
@@ -1,23 +1,51 @@
 // <!-- Initialize Swiper -->
 var swiper = new Swiper(".swiper-container", {
 	cssMode: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 	mousewheel: true,
 	keyboard: true,
 	updateOnWindowResize: true,
-	slidesPerView: 3,
 	loop: true,
-	spaceBetween: 10,
 	autoplay: {
 		delay: 5000,
-	 },
-	 preloadImages: false,
-	 lazy: true,
+	},
+	disableOnInteraction: false,
+	waitForTransition: false,
+	preloadImages: false,
+	lazy: true,
+	slidesPerView: 2,
+	breakpoints: {
+		"@0.00": {
+			slidesPerView: 2,
+			spaceBetween: 16,
+			slidesPerGroup: 2,
+		},
+		"@0.45": {
+			slidesPerView: 3,
+			spaceBetween: 32,
+			slidesPerGroup: 3,
+		},
+		"@0.75": {
+			slidesPerView: 4,
+			spaceBetween: 20,
+			slidesPerGroup: 3,
+		},
+		"@1": {
+			slidesPerView: 6,
+			spaceBetween: 20,
+			slidesPerGroup: 3,
+		},
+		"@1.2": {
+			slidesPerView: 6,
+			spaceBetween: 30,
+			slidesPerGroup: 3,
+		},
+	},
 });
