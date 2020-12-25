56500453dd94cc12774cc297aa5a6cb6a8e9d306
Mirodil Kamilov <mirodilkamilov1999@gmail.com>
2020-12-25 23:18:14 +0500

    tested Swiper

--- root/js/carousel.js
+++ root/js/carousel.js
@@ -1,22 +1,23 @@
-$(document).ready(function () {
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: true,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 2
-            }
-        }]
-    });
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".swiper-container", {
+	cssMode: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	mousewheel: true,
+	keyboard: true,
+	updateOnWindowResize: true,
+	slidesPerView: 3,
+	loop: true,
+	spaceBetween: 10,
+	autoplay: {
+		delay: 5000,
+	 },
+	 preloadImages: false,
+	 lazy: true,
 });
