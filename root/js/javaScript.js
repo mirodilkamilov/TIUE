7e70065e91c597f0285326b4ffab167c5547ea45
Mirodil Kamilov <mirodilkamilov1999@gmail.com>
2020-12-27 12:58:20 +0500

    scrollTrigger for mobile (testing)

--- root/js/javaScript.js
+++ root/js/javaScript.js
@@ -66,69 +66,202 @@ $(document).ready(function () {
 
 //? ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.matchMedia({
+	"(max-width: 767px)": function () {
+		let carousel = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".main-carousel",
+				start: "top center",
+			},
+		});
+		carousel
+			.from(".slide1 p, .slide1 h3, .slide1 .read-more-btn", { x: -100, opacity: 0, duration: 0.7 });
 
-let carousel = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".main-carousel",
-		start: "top center",
+		let about_first = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".about",
+				start: "-=150px center",
+			},
+		});
+		about_first.from(".about-top-content", {
+			x: -100,
+			opacity: 0,
+			duration: 0.7,
+		});
+
+		let rector = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".rector-container",
+				start: "-=150px center",
+			},
+		});
+		rector.from(".rector-container", {
+			x: -100,
+			opacity: 0,
+			duration: 0.7,
+		});
+
+		let vision = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".vision",
+				start: "-=150px center",
+			},
+		});
+		vision.from(".vision", { x: -100, opacity: 0, duration: 0.7 });
+
+		let mission = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".mission",
+				start: "-=150px center",
+			},
+		});
+		mission.from(
+			".mission",
+			{ x: 100, opacity: 0, duration: 0.7 },
+			"-=0.5"
+		);
+
+		let values = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".values",
+				start: "-=150px center",
+			},
+		});
+		values
+			.from(".values .sub-heading", {
+				x: -100,
+				opacity: 0,
+				duration: 0.7,
+			})
+			.from(
+				".card-container",
+				{ x: -100, opacity: 0, duration: 0.7 },
+				"-=0.3"
+			)
+			.from(".last-card", { x: -100, opacity: 0, duration: 0.7 });
+
+		let team = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".core-team",
+				start: "top center",
+			},
+		});
+		team.from(".core-team .heading", { x: -100, opacity: 0, duration: 0.7 })
+			.from(".leadership", { x: 100, opacity: 0, duration: 0.7 }, "-=0.5")
+			.from(".t-hub", { x: -100, opacity: 0, duration: 0.7 }, "-=0.5");
+
+		let team_last = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".elp-team",
+				start: "top center",
+				markers: true,
+			},
+		});
+		team_last
+			.from(".elp-team", { x: 100, opacity: 0, duration: 0.7 }, "-=0.5")
+			.from(".physics", { x: -100, opacity: 0, duration: 0.7 }, "-=0.5");
+
+		let programs = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".programs",
+				start: "-=150px center",
+			},
+		});
+		programs
+			.from(".programs .heading", { x: -100, opacity: 0, duration: 0.7 })
+			.from(
+				".program-card-container",
+				{ y: 100, opacity: 0, duration: 0.7 },
+				"-=0.5"
+			);
+	},
+
+	"(min-width: 1200px)": function () {
+		let carousel = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".main-carousel",
+				start: "top center",
+			},
+		});
+		carousel.from(".slide1", { x: -100, opacity: 0, duration: 0.7 });
+
+		let about_first = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".about",
+				start: "-=150px center",
+			},
+		});
+		about_first.from(".about-top-content", {
+			x: -100,
+			opacity: 0,
+			duration: 0.7,
+		});
+
+		let rector = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".rector-container",
+				start: "-=150px center",
+			},
+		});
+		rector.from(".rector-container", {
+			x: -100,
+			opacity: 0,
+			duration: 0.7,
+		});
+
+		let vision = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".vision-mission-container",
+				start: "-=150px center",
+			},
+		});
+		vision
+			.from(".vision", { x: -100, opacity: 0, duration: 0.7 })
+			.from(".mission", { x: 100, opacity: 0, duration: 0.7 }, "-=0.5");
+
+		let values = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".values",
+				start: "-=150px center",
+			},
+		});
+		values
+			.from(".values .sub-heading", {
+				y: -100,
+				opacity: 0,
+				duration: 0.7,
+			})
+			.from(
+				".card-container",
+				{ y: -100, opacity: 0, duration: 0.7 },
+				"-=0.3"
+			)
+			.from(".last-card", { y: -100, opacity: 0, duration: 0.7 });
+
+		let team = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".core-team",
+				start: "-=150px center",
+			},
+		});
+		team.from(".core-team .heading", { x: -100, opacity: 0, duration: 0.7 })
+			.from(".leadership", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
+			.from(".t-hub", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
+			.from(".elp-team", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
+			.from(".physics", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5");
+
+		let programs = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".programs",
+				start: "-=150px center",
+			},
+		});
+		programs
+			.from(".programs .heading", { x: -100, opacity: 0, duration: 0.7 })
+			.from(
+				".program-card-container",
+				{ y: 100, opacity: 0, duration: 0.7 },
+				"-=0.5"
+			);
 	},
 });
-carousel.from(".slide1", { x: -100, opacity: 0, duration: 0.7 });
-
-let about_first = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".about",
-		start: "-=150px center",
-	},
-});
-about_first.from(".about-top-content", { x: -100, opacity: 0, duration: 0.7 });
-
-let rector = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".rector-container",
-		start: "-=150px center",
-	},
-});
-rector.from(".rector-container", { x: -100, opacity: 0, duration: 0.7 });
-
-let vision = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".vision-mission-container",
-		start: "-=150px center",
-	},
-});
-vision
-	.from(".vision", { x: -100, opacity: 0, duration: 0.7 })
-	.from(".mission", { x: 100, opacity: 0, duration: 0.7 }, "-=0.5");
-
-let values = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".values",
-		start: "-=150px center",
-	},
-});
-values
-	.from(".values .sub-heading", { x: -100, opacity: 0, duration: 0.7 })
-	.from(".card-container", { x: -100, opacity: 0, duration: 0.7 }, "-=0.3")
-	.from(".last-card", { x: -100, opacity: 0, duration: 0.7 });
-
-let team = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".core-team",
-		start: "-=150px center",
-	},
-});
-team.from(".core-team .heading", { x: -100, opacity: 0, duration: 0.7 })
-	.from(".leadership", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
-	.from(".t-hub", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
-	.from(".elp-team", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5")
-	.from(".physics", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5");
-
-let programs = gsap.timeline({
-	scrollTrigger: {
-		trigger: ".programs",
-		start: "-=150px center",
-	},
-});
-programs.from(".programs .heading", { x: -100, opacity: 0, duration: 0.7 })
-	.from(".program-card-container", { y: 100, opacity: 0, duration: 0.7 }, "-=0.5");
